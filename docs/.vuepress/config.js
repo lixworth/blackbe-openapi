99272ad764e272ef26b860192a39c00c2b14987d
LixWorth <lixworth@gmail.com>
2020-08-19 08:16:32 +0800

    fix sth ”

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'BlackBE',
     description: 'BlackBE APIDOC V2',
     themeConfig: {
-        logo: '//api.blackbe.xyz:8890/logo-tm.png',
+        logo: '/logo-tm.png',
         nav: [
             { text: '介绍', link: '/' },
             { text: '文档', link: '/guide/' },
